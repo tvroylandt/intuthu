b38e2a2bccff73938eb8f587e2c98879acff1c35
Thomas Vroylandt <tvroylandt@gmail.com>
2020-07-26 22:02:54 +0200

    Pagedown skeleton court

--- R/paged_template.R
+++ R/paged_template.R
@@ -22,3 +22,26 @@ sgdf_paged_long <- function(toc = FALSE,
     css = c(css_sgdf, css_sgdf_paged_long)
   )
 }
+
+#' Rapport PDF - format note courte
+#'
+#' @param picto Picto a inclure en premiere page
+#'
+#' @return Un format Rmd sur mesure
+#' @export
+#'
+sgdf_paged_short <- function(picto) {
+  # fichiers de style
+  css_sgdf <- pkg_resource("stylesheets", "sgdf.css")
+  css_sgdf_paged_short <-
+    pkg_resource("stylesheets", "custom-page_sgdf.css")
+
+  # template
+  pagedown::html_paged(
+    toc = FALSE,
+    fig_width = 6.5,
+    fig_height = 4,
+    theme = NULL,
+    css = c(css_sgdf, css_sgdf_paged_short)
+  )
+}
