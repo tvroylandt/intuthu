e8c329e4bd0cbc9cba83c395dd83440ed948493c
Thomas Vroylandt <tvroylandt@gmail.com>
2020-08-02 20:45:40 +0200

    Custom format flex

--- R/flex_template.R
+++ R/flex_template.R
@@ -1,3 +1,22 @@
+#' Tableau de bord flexdashboard SGDF
+#'
+#' @return Format flex SGDF
+#' @export
+#'
 sgdf_flexdashboard <- function(){
+  # fichiers de style
+  css_sgdf <- pkg_resource("stylesheets", "sgdf_style.css")
+  css_sgdf_flex <-
+    pkg_resource("stylesheets", "sgdf_flex.css")
 
+  logo_sgdf <- pkg_resource("logo", "logo_sgdf_instit.png")
+
+  # template
+  flexdashboard::flex_dashboard(
+    logo = logo_sgdf,
+    fig_width = 6.5,
+    fig_height = 4,
+    theme = NULL,
+    css = c(css_sgdf, css_sgdf_flex)
+  )
 }
